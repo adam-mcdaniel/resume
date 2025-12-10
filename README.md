99c3385da786f0f4fb2f0480c5c15412c3a555d1
Adam McDaniel <adam.mcdaniel17@gmail.com>
2025-12-10 14:00:25 -0500

    Changed build to automatically convert PDFs

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 
 # Curriculum Vitae
 
+[![Curriculum Vitae](cv/CV-0.png)](cv/CV.pdf)
 [![Curriculum Vitae](cv/CV-1.png)](cv/CV.pdf)
 [![Curriculum Vitae](cv/CV-2.png)](cv/CV.pdf)
 [![Curriculum Vitae](cv/CV-3.png)](cv/CV.pdf)
--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# First convert the pdfs to images of each page for the website
+
+# Do this with imagemagick
+magick -density 300 cv/*.pdf -depth 8 -strip -background white -alpha off cv/CV.png
+magick -density 300 resume/*.pdf -depth 8 -strip -background white -alpha off resume/Resume.png
+
 rm -rf docs/cv docs/resume
 cp -r cv docs/
 cp -r resume docs/
